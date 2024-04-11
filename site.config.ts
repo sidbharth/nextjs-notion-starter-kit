77a07dd18e7d9553163474dd3bac454652055f5c
Siddharth Biju <hello@sidb.me>
2024-04-11 22:17:05 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'sidharth biju',
+  name: 'siddharth biju',
   domain: 'sidb.me',
   author: 'siddharth biju',
 
